97663a63a5fe7a189ae14b12deb7eb80daa50d1f
munisalesforce <110750661+munisalesforce@users.noreply.github.com>
2022-12-20 12:07:54 -0500

    Update CamelConfig.java
    Added backendMathService to  invoke  original service exposed on port 8080

--- camel-proxy-server/src/main/java/com/kp/swasthik/CamelConfig.java
+++ camel-proxy-server/src/main/java/com/kp/swasthik/CamelConfig.java
@@ -21,4 +21,17 @@ public class CamelConfig {
 		ep.setWsdlURL("http://localhost:8080/services/math?wsdl");
 		return ep;
 	}
+	
+	@Bean
+	public CxfEndpoint backendMathService(Bus bus, CamelContext context) {
+		CxfEndpoint ep = new CxfEndpoint();
+		ep.setAddress("http://localhost:8080/services/math");
+		ep.setBus(bus);
+		ep.setCamelContext(context);
+		ep.setLoggingFeatureEnabled(true);
+		ep.setDataFormat(DataFormat.PAYLOAD);
+		ep.setWsdlURL("http://localhost:8080/services/math?wsdl");
+		return ep;
+	}
 }
+
